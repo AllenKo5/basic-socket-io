dfe58d4e3f7e7b28574074ee4953149ba34ab7f2
Allen Ko (RIT Student) <ask9458@rit.edu>
2022-11-07 15:47:55 -0500

    set up socket io

--- hosted/client.js
+++ hosted/client.js
@@ -1,5 +1,54 @@
-const init = () => {
+const socket = io();
 
+const handleEditForm = () => {
+    const editForm = document.getElementById('editForm');
+    const editBox = document.getElementById('editBox');
+    const channelSelect = document.getElementById('channelSelect');
+
+    editForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        if (editBox.value) {
+            const data = {
+                message: editBox.value,
+                channel: channelSelect.value,
+            }
+
+            socket.emit('chat message', data);
+            editBox.value = '';
+        }
+    });
+}
+
+const handleChatMessage = (msg) => {
+    const messageDiv = document.createElement('div');
+    messageDiv.innerText = msg;
+    document.getElementById('messages').appendChild(messageDiv);
+};
+
+const handleChannelSelect = () => {
+    const channelSelect = document.getElementById('channelSelect');
+    const messages = document.getElementById('messages');
+
+    channelSelect.addEventListener('change', () => {
+        messages.innerHTML = '';
+
+        switch (channelSelect.value) {
+            case 'memes':
+                socket.off('general');
+                socket.on('memes', handleChatMessage);
+                break;
+            default:
+                socket.off('memes');
+                socket.on('general', handleChatMessage);
+                break;
+        };
+    });
+};
+
+const init = () => {
+    handleEditForm();
+    handleChannelSelect();
 };
 
 window.onload = init;
